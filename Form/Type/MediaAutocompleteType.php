b03f19022ecfac1a72d2cd3400773d24740d917a
Robert <robertslootjes@gmail.com>
2017-01-25 16:31:48 +0100

    Add autocomplete form type with image preview

---
+++ Form/Type/MediaAutocompleteType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace MediaMonks\SonataMediaBundle\Form\Type;
+
+use Sonata\AdminBundle\Form\Type\ModelAutocompleteType as BaseModelAutocompleteType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Templating\EngineInterface;
+
+class MediaAutocompleteType extends BaseModelAutocompleteType
+{
+    /**
+     * @var EngineInterface
+     */
+    private $templateEngine;
+
+    /**
+     * @param EngineInterface $templateEngine
+     */
+    public function __construct(EngineInterface $templateEngine)
+    {
+        $this->templateEngine = $templateEngine;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults(
+            [
+                'property' => 'title',
+                'to_string_callback' => function ($media, $property) {
+                    return $this->templateEngine->render(
+                        'MediaMonksSonataMediaBundle:MediaAdmin:autocomplete.html.twig',
+                        [
+                            'media' => $media,
+                        ]
+                    );
+                },
+            ]
+        );
+    }
+}
